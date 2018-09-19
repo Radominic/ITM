a7c141328d6f521986768b114a12ea6bb8f26f6c
BGM <wjdrmf314@gmail.com>
2018-09-19 23:30:10 +0900

    first commit

---
+++ app/src/main/java/com/example/user/gjsd/ListViewItem.java
@@ -0,0 +1,30 @@
+package com.example.user.gjsd;
+
+import android.graphics.drawable.Drawable;
+
+public class ListViewItem {
+    private Drawable iconDrawable ;
+    private String titleStr ;
+    private int checbox;
+
+    public void setIcon(Drawable icon) {
+        iconDrawable = icon ;
+    }
+    public void setTitle(String title) {
+        titleStr = title ;
+    }
+    public void setChecbox(int num){
+        checbox = num;
+    }
+
+
+    public Drawable getIcon() {
+        return this.iconDrawable ;
+    }
+    public String getTitle() {
+        return this.titleStr ;
+    }
+    public int getChecbox(){
+        return this.checbox;
+    }
+}
--- app/src/main/java/com/example/user/gjsd/MainActivity.java
+++ app/src/main/java/com/example/user/gjsd/MainActivity.java
@@ -5,19 +5,29 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.SlidingDrawer;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     SlidingDrawer drawer1,drawer2;
     Animation animro, animri, animlo, animli;
+    String itemname = "default";
+    String[] item = {"동태","조기","달걀","닭고기","돼지고기","쇠고기","애호박","오이","상추","양파","무","배추","배","사과","오징어","고등어","명태","호박","냉동참조기"};
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -62,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        drawer1 = (SlidingDrawer)findViewById(R.id.slide1);
+        drawer1 = (SlidingDrawer) findViewById(R.id.slide1);
         drawer2 = (SlidingDrawer)findViewById(R.id.slide2);
         TextView tv1 = (TextView)findViewById(R.id.title1);
         tv1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/Roboto-Black.ttf"));
@@ -72,6 +82,30 @@ public class MainActivity extends AppCompatActivity {
         TextView tv22 = (TextView)findViewById(R.id.title22);
         tv11.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/Roboto-Black.ttf"));
         tv22.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/Roboto-Black.ttf"));
+
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        adapter = new ListViewAdapter() ;
+
+        listview = (ListView) findViewById(R.id.itemlist1);
+        listview.setAdapter(adapter);
+
+        // 첫 번째 아이템 추가.
+
+       for(int i=0;i<item.length;i++){
+           adapter.addItem(ContextCompat.getDrawable(this, R.drawable.banana),
+                   item[i]) ;
+       }
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                itemname = item[position];
+                Log.i("itemname : ", itemname);
+                drawer1.animateClose();
+            }
+        }) ;
 
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
